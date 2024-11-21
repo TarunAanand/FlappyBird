a9eedfb4b4a73b7c7a7218b7f16785354f2d9ce2
tarun <tarunaananad27@gmail.com>
2024-11-21 13:39:14 +0530

    Intial commit

--- src/App.java
+++ src/App.java
@@ -4,7 +4,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         int boardWidth = 360;
         int boardHeight = 640;
-
+ 
         JFrame frame = new JFrame("Flappy Bird");
         frame.setSize(boardWidth, boardHeight);
         frame.setLocationRelativeTo(null);
